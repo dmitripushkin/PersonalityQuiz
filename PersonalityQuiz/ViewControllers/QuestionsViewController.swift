ebb4616ea63be66ff22fd0ad90eda67c8ba82ba5
Dmitri Gordeev <dimapushkin@gmail.com>
2021-09-02 04:00:05 +0300

    done some code

--- PersonalityQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalityQuiz/ViewControllers/QuestionsViewController.swift
@@ -42,7 +42,8 @@ class QuestionsViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        guard let resultVC = segue.destination as? ResultViewController else { return }
+        resultVC.chosenAnswers = answersChosen
     }
     
     @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
